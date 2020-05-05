2f69f6c9cc960df91b187f0aa261b00df5528883
defuj <defuj.official@gmail.com>
2020-05-05 22:37:22 +0700

    update

--- app/src/main/java/id/co/bigtek/defuj/ActivityMain.kt
+++ app/src/main/java/id/co/bigtek/defuj/ActivityMain.kt
@@ -1,13 +1,13 @@
 package id.co.bigtek.defuj
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import cn.pedant.SweetAlert.SweetAlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class ActivityMain : AppCompatActivity() {
-
+    var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,7 +21,28 @@ class ActivityMain : AppCompatActivity() {
         }
 
         btndetail!!.setOnClickListener {
-            startActivity(Intent(this,ActivityProfile::class.java))
+            if(count == 3){
+                startActivity(Intent(this,ActivityProfile::class.java))
+            }else{
+                if(count == 0){
+                    confirm("Baru Mulai","Klik Ok dan ulangi",SweetAlertDialog.WARNING_TYPE)
+                }else if(count == 1){
+                    confirm("Yang kedua","Klik Ok dan ulangi",SweetAlertDialog.WARNING_TYPE)
+                }else{
+                    confirm("Yang terakhir","Klik Ok dan ulangi",SweetAlertDialog.WARNING_TYPE)
+                }
+            }
         }
+    }
+
+    private fun confirm(title : String,content : String, type : Int){
+        val dialog = SweetAlertDialog(this,SweetAlertDialog.WARNING_TYPE)
+        dialog.titleText = title
+        dialog.contentText = content
+        dialog.setConfirmClickListener {
+            count +=1
+            dialog.dismissWithAnimation()
+        }
+        dialog.show()
     }
 }
--- app/src/main/java/id/co/bigtek/defuj/ActivityProfile.kt
+++ app/src/main/java/id/co/bigtek/defuj/ActivityProfile.kt
@@ -2,10 +2,12 @@ package id.co.bigtek.defuj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_profile.*
 
 class ActivityProfile : AppCompatActivity() {
-
+    private var count = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
@@ -15,7 +17,29 @@ class ActivityProfile : AppCompatActivity() {
 
     private fun functions() {
         toolbarDetails!!.setNavigationOnClickListener {
+            back()
+        }
+    }
+
+    override fun onBackPressed() {
+        back()
+    }
+
+    private fun back(){
+        if(count == 0){
             finish()
+        }else{
+            count -=1
+            Toast.makeText(this,"Tekan sekali lagi untuk keluar",Toast.LENGTH_LONG).show()
+            object : CountDownTimer(3000,1000){
+                override fun onFinish() {
+                    count = 1
+                }
+
+                override fun onTick(millisUntilFinished: Long) {
+
+                }
+            }.start()
         }
     }
 }
